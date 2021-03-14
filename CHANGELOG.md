4a71848febe6796559c504ac55262d8b119831c4
deepcoder42 <deepcoder42@gmail.com>
2021-03-14 21:03:37 +0000

    Project initialization

---
+++ CHANGELOG.md
@@ -0,0 +1,147 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on "Keep a Changelog".  This project adheres to Semantic Versioning.
+
+
+## [3.3.0] - 2020-11-06
+- Updated to use the mysql_libs v5.0.0 library.
+
+### Fixed
+- config/mysql.cfg.TEMPLATE:  Point to correct socket file.
+- run_program:  Remove "-r" from command line if database not GTID enabled.
+
+### Changed
+- set_db_list:  Replace multiple returns with single return.
+- run_program:  Changed default value of dmp_path to None.
+- main:  Made "-o" a required argument for "-A", "-B", and "-D" options.
+- config/mysql_cfg.py.TEMPLATE:  Changed entry name.
+- Documentation updates.
+
+
+## [3.2.0] - 2020-07-06
+### Fixed
+- main: Fixed handling command line arguments from SonarQube scan finding.
+
+### Added
+- Added email capability for error output.
+- Added standard error out redirection -w option.
+
+### Changed
+- dump_db:  Add email capability for dumps error file.
+- run_program:  Setup and configured email.
+- main:  Added -e and -t options to parsing for email capability.
+- set_db_list:  Changed variable to standard naming convention.
+- dump_db:  Changed variable to standard naming convention.
+- main:  Remove non-used argument in run_program call.
+- dump_run:  Setup subprocess.Popen with stderr option and pass error file to the option.
+- dump_db:  Set up and open error file if error suppression is passed.
+- run_program: Parse the -w option.
+- run_program: Refactored check on -z option in args_array.
+- config/mysql.cfg.TEMPLATE:  Changed format of file.
+- dump_run:  Replaced cmds_gen.run_prog with "with open" and "subprocess.Popen" code.
+- Documentation updates.
+
+
+## [3.1.0] - 2020-01-17
+### Fixed
+- crt_dump_cmd:  Fixed problem with mutable default arguments issue.
+- dump_run:  Fixed problem with mutable default arguments issue.
+- dump_db:  Fixed problem with mutable default arguments issue.
+- set_db_list:  Fixed problem with mutable default arguments issue.
+- run_program:  Fixed problem with mutable default arguments issue.
+
+### Changed
+- main:  Added program lock functionality to program.
+- main:  Added new option -y to the program.
+- crt_dump_cmd:  Changed variable name to standard convention.
+- set_db_list:  Changed variable name to standard convention.
+- run_program:  Changed variable name to standard convention.
+- main:  Refactored and streamlined "if" statements.
+- Added \*\*kwargs to those function parameter lists without the keyword argument capability.
+- Documentation updates.
+
+
+## [3.0.1] - 2018-12-06
+### Changed
+- Documentation updates.
+
+
+## [3.0.0] - 2018-05-23
+Breaking Change
+
+### Changed
+- Changed "mysql_libs" calls to new naming schema.
+- Changed "cmds_gen" calls to new naming schema.
+- Changed "gen_libs" calls to new naming schema.
+- Changed "arg_parser" calls to new naming schema.
+- Changed function names from uppercase to lowercase.
+- Setup single-source version control.
+
+
+## [2.7.0] - 2018-05-11
+### Changed
+- Changed "server" to "mysql_class" module reference.
+- Changed "commands" to "mysql_libs" module reference.
+
+### Added
+- Added single-source version control.
+
+
+## [2.6.0] - 2017-08-18
+### Changed
+- Convert program to use local libraries from ./lib directory.
+- Change single quotes to double quotes.
+- Help_Message:  Replace docstring with printing the programs \_\_doc\_\_.
+
+
+## [2.5.0] - 2016-12-19
+### Changed
+- Set_Db_List:  Modified -B section to handle multiple databases.  Replaced sys.exit() with empty list.
+- Dump_DB:  Added elif to check for all-databases run and modified else to assume empty database list and there is nothing to dump or missing -D option.
+- Run_Program:  Removed if statement and call Set_Db_List function regardlessly, also added \*\*kwargs to argument list to Set_Db_List call.
+- main:  Added opt_multi_list variable.
+- Added option for -B option to have multiple database names and also corrected an error if no option selected the program crashed trying to access the database name under -B option.
+
+
+## [2.4.0] - 2016-10-19
+### Changed
+- Crt_Dump_Cmd:  Replaced prog_gen.Is_Add_Cmd with cmds_gen.Is_Add_Cmd and Replaced prog_gen.Add_Cmd with cmds_gen.Add_Cmd.
+- Run_Program:  Determine if database list creation required.  Set the Servers class GTID mode.  Remove --set-gtid-purged for databases not GTID enabled.
+- Dump_Db:  If database list is empty, then run dump command without database names.  Replaced prog_gen.Add_Cmd with cmds_gen.Add_Cmd.
+- main:  Replaced Arg_Req_Xor with Arg_Xor_Dict and added -D option arguments to opt_dump_list variable.
+- MySQL 5.6 (GTID Enabled) gives warning if a database is dumped with GTID entries.  Added two new options (-D and -r) to purge the GTID entries from the dump file and/or dump all of the databases to a single dump file and this will drop the warning message.
+
+### Added
+- Dump_Cmd function.
+
+
+## [2.3.0] - 2016-10-18
+### Changed
+- main:  Changed -b to -B and -a to -A.
+- Set_Db_List:  Changed -b to -B.
+- Bring the main argument options in-line with the other programs.
+
+
+## [2.2.0] - 2016-10-12
+### Changed
+- Run_Program:  Replaced Crt_Srv_Inst with Create_Instance to allow for --defaults-extra-file option.
+- MySQL 5.6 now gives warning if password is passed on the command line.  To suppress this warning, will require the use of the --defaults-extra-file option.  This will require the use of updated commands library and server class files.  See in documentation above for exact version required for MySQL 5.6.
+
+
+## [2.1.0] - 2016-09-13
+### Changed
+- Dump_Db:  Replaced my_prog.Append_Cmd with prog_gen.Add_Cmd.  Replaced my_prog.Run_Prog_2_File with prog_gen.Run_Prog.
+- Crt_Dump_Cmd:  Replaced my_prog.Crt_Cmd with commands.Crt_Cmd.  Replaced my_prog.Append_Cmd with prog_gen.Add_Cmd.  Replaced my_prog.Is_Add_Cmd with prog_gen.Is_Add_Cmd.
+- main:  Replaced Arg_Parse with Arg_Parse2, reorganized the main 'if' statements, and streamlined the check process.
+- Set_Db_List:  Added connect and disconect commands to the database.
+
+
+## [2.0.0] - 2015-12-04
+### Changed
+- Extensive updates to the program to modularize and streamline the program and also replace the current database connection mechanism with a class based database connection mechanism.
+
+
+## [1.0.0] - 2015-10-08
+- Initial creation.
+
---
+++ test/unit/mysql_db_dump/run_program.py
@@ -0,0 +1,548 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  run_program.py
+
+    Description:  Unit testing of run_program in mysql_db_dump.py.
+
+    Usage:
+        test/unit/mysql_db_dump/run_program.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_db_dump
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for mysql_class.Server class.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.gtid_mode = True
+        self.name = "ServerName"
+
+    def connect(self):
+
+        """Method:  connect
+
+        Description:  Stub method holder for mysql_class.Server.connect.
+
+        Arguments:
+
+        """
+
+        pass
+
+    def set_srv_gtid(self):
+
+        """Method:  set_srv_gtid
+
+        Description:  Stub method holder for mysql_class.Server.set_srv_gtid.
+
+        Arguments:
+
+        """
+
+        pass
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_multiple_options2 -> Test with multiple options passed 2.
+        test_multiple_options -> Test with multiple options passed.
+        test_no_email -> Test with no email configured.
+        test_email_subj -> Test with subject line passed.
+        test_email_no_subj -> Test with no subject line passed.
+        test_email -> Test with email configured.
+        test_w_option2 -> Test with -w option set to True.
+        test_w_option -> Test with -w option set to False.
+        test_r_option_miss -> Test with -r option value not in command list.
+        test_r_option3 -> Test with -r option and GTID set to False.
+        test_r_option2 -> Test with -r option and GTID set to False.
+        test_r_option -> Test with -r option and GTID set to True.
+        test_z_option2 -> Test with -z option set to False.
+        test_z_option -> Test with -z option set to True.
+        test_no_o_option -> Test with no -o option.
+        test_o_option -> Test with -o option.
+        test_run_program -> Test run_program with default settings.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        gtid_arg = "--set-gtid-purged=OFF"
+        self.server = Server()
+        self.dump_cmd = ["dump_command", gtid_arg]
+        self.dump_cmd2 = ["dump_command"]
+        self.dump_cmd3 = ["dump_command", gtid_arg]
+        self.db_list = []
+        self.args_array = {"-c": "config", "-d": "/dir"}
+        self.args_array2 = {"-c": "config", "-d": "/dir", "-o": "/dir/path"}
+        self.args_array3 = {"-c": "config", "-d": "/dir", "-z": True}
+        self.args_array4 = {"-c": "config", "-d": "/dir", "-z": False}
+        self.args_array5 = {"-c": "config", "-d": "/dir", "-r": True}
+        self.args_array6 = {"-c": "config", "-d": "/dir", "-w": True}
+        self.args_array7 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"]}
+        self.args_array8 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"],
+                            "t": ["Subject", "Line"]}
+        self.args_array9 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"],
+                            "t": ["Subject", "Line"], "-z": True}
+        self.args_array10 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"],
+                             "t": ["Subject", "Line"], "-z": True, "-w": True}
+        self.opt_arg_list = ["--ignore-table=mysql.event"]
+        self.opt_dump_list = {
+            "-s": "--single-transaction",
+            "-D": ["--all-databases", "--triggers", "--routines", "--events"],
+            "-r": gtid_arg}
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_multiple_options2(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_multiple_options2
+
+        Description:  Test with multiple options passed 2.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array10, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_multiple_options(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_multiple_options
+
+        Description:  Test with multiple options passed.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array9, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_no_email(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_no_email
+
+        Description:  Test with no email configured.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email_subj(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email_subj
+
+        Description:  Test with subject line passed.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array8, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email_no_subj(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email_no_subj
+
+        Description:  Test with no subject line passed.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array7, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email
+
+        Description:  Test with email configured.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array7, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_w_option2(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_w_option2
+
+        Description:  Test with -w option set to True.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array6, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_w_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_w_option
+
+        Description:  Test with -w option set to False.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_r_option_miss(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_r_option_miss
+
+        Description:  Test with -r option value not in command list.
+
+        Arguments:
+
+        """
+
+        self.server.gtid_mode = False
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd2
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_r_option3(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_r_option3
+
+        Description:  Test with -r option and GTID set to False.
+
+        Arguments:
+
+        """
+
+        self.server.gtid_mode = False
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd3
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_r_option2(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_r_option2
+
+        Description:  Test with -r option and GTID set to False.
+
+        Arguments:
+
+        """
+
+        self.server.gtid_mode = False
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_r_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_r_option
+
+        Description:  Test with -r option and GTID set to True.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_z_option2(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_z_option2
+
+        Description:  Test with -z option set to False.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array4, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_z_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_z_option
+
+        Description:  Test with -z option set to True.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array3, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_no_o_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_no_o_option
+
+        Description:  Test with no -o option.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_o_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_o_option
+
+        Description:  Test with -o option.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array2, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_run_program(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_run_program
+
+        Description:  Test run_program with default settings.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
+
+
+if __name__ == "__main__":
+    unittest.main()
